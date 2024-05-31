51c119a8209cf02c1606d47ba494d9aae994868d
Mohammed Alhaddar <moalhaddar@gmail.com>
2024-05-31 13:22:42 +0300

    Added cross origin on the pdf endpoint

--- src/main/kotlin/dev/alhaddar/docxtopdf/rest/PdfController.kt
+++ src/main/kotlin/dev/alhaddar/docxtopdf/rest/PdfController.kt
@@ -4,6 +4,7 @@ import dev.alhaddar.docxtopdf.logger
 import dev.alhaddar.docxtopdf.service.UnoService
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
@@ -14,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile
 class PdfController(val unoService: UnoService) {
     val logger = logger()
 
+    @CrossOrigin
     @RequestMapping(value= ["/pdf", "/docx-to-pdf"], method = [RequestMethod.POST])
     fun getPdf(@RequestParam("document") file: MultipartFile): ResponseEntity<ByteArray>{
         logger.info("PDF Request. Docx file size: ${file.inputStream.readBytes().size} bytes.")
